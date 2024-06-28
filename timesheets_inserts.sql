baa9318cdb5af228d356e4d4bae98c511fa20829
beccac87 <58445123+beccac87@users.noreply.github.com>
2024-06-28 00:25:12 -0400

    Add files via upload

---
+++ timesheets_inserts.sql
@@ -0,0 +1,54 @@
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+1,
+35
+)
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+2,
+35
+)
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+3,
+35
+)
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+4,
+35
+)
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+5,
+35
+)
+insert into fudgemart_tiemsheet (
+timesheet_payrolldate,
+timesheet_employee_id,
+timesheet_hours )
+values (
+'6/7/2020',
+6,
+35
+)
