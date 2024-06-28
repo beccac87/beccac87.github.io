67a4ee4c2c32d686fe436564a9e219ea64a8bcec
beccac87 <58445123+beccac87@users.noreply.github.com>
2024-06-28 00:26:06 -0400

    Add files via upload

---
+++ timesheet_update_e.sql
@@ -0,0 +1,7 @@
+update fudgemart_employees
+set employee_lastname ='Dente'
+where employee_lastname='Hvmeehom'
+update fudgemart_employees
+set employee_firstname ='AL'
+where employee_firstname='Lee'
+select * from fudgemart_employees
